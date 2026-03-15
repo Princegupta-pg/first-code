ccfd219d285123bac9d977a371c20133aa16647f
Prince Gupta <pg198798@gmail.com>
2026-03-15 12:48:22 +0530

    Add files via upload

---
+++ Pseries.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Pseries {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0.0;
+        for(int i = 1; i<=n; i++){
+            sum = sum + (1.0/(i*i));
+        }
+        System.out.println(sum);
+    }
+}
